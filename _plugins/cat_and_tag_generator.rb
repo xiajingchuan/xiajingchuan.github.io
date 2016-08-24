2247e4649f36ff785d4e64e18d063647428253b8
panke <xiajingchuan@126.com>
2016-08-24 16:40:50 +0800

    add _site

--- _plugins/cat_and_tag_generator.rb
+++ _plugins/cat_and_tag_generator.rb
@@ -61,7 +61,7 @@ module Jekyll
       @site = site
       @base = base
       @dir = dir
-      @name = 'atom.xml'
+      @name = 'atom.xml' 
 
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), "group_atom.xml")
